9ef5daf55688a3a986b4248be8c9b362abeddb04
amarrerod <alemarrerodiaz@gmail.com>
2022-10-07 14:53:08 +0100

    🚚 Updating structure

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use crate::search::Nucleotide;
-
 extern crate itertools;
-
+mod genes;
 mod search;
 
-use search::Codon;
+use genes::{Codon, Nucleotide};
 
 fn main() {
     println!("Hello, world!");
     let gene_str: String = String::from("ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT");
-    let gene = search::string_to_gene(&gene_str);
+    let gene = genes::string_to_gene(&gene_str);
 
     let codon1 : Codon = (Nucleotide::A, Nucleotide::C, Nucleotide::G);
     let codon2 : Codon = (Nucleotide::G, Nucleotide::A, Nucleotide::T);
--- src/search/mod.rs
+++
@@ -1,9 +0,0 @@
-
-
-mod dna_search;
-mod genes;
-
-pub use genes::Nucleotide;
-pub use genes::Codon;
-pub use genes::string_to_gene;
-pub use dna_search::linear_search;
